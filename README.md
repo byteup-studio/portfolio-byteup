d6de14319bafdaf02097c6aba2789d7da79adbd1
byteup-studio <byteupcontato@gmail.com>
2025-08-05 10:46:07 -0300

    Versão inicial do portfólio Byte Up

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1 align="center">Portifólio - Byte Up 💻</h1>
---
+++ assets/js/projetos.js
@@ -0,0 +1,35 @@
+function projetos(section) {
+  section.innerHTML = `<header>
+      <h2>Projetos</h2>
+    </header>
+    <div class="content">
+      <div class="cards">
+        <div></div>
+        <div class="card">
+          <h3>Em breve...</h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          
+            />
+          </svg>
+          <p>
+            Aprendizado constante! <br />
+            Em breve muitos outros projetos.
+          </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            />
+          </svg>
+        </div>
+        <div></div>
+      </div>
+    </div>`;
+}
+
+export { projetos };
